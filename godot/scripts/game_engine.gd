e3d7c5acef6e3c1219dd61f495cef35ef87f7f60
Douglas Gabriel da Costa Alves <104402297+Kirikazu22@users.noreply.github.com>
2025-07-16 11:04:58 -0300

    Add files via upload

--- godot/scripts/game_engine.gd
+++ godot/scripts/game_engine.gd
@@ -27,11 +27,8 @@ func send_python_code(code: String, fase):
 	var body = JSON.stringify({"code": code, "fase": fase})
 	var headers = ["Content-Type: application/json"]
 
-	var url = ""
-	if OS.has_feature("HTML5"):
-		url = "https://godot-python-game.onrender.com/execute_code"
-	else:
-		url = "http://127.0.0.1:8000/execute_code"
+	# alternativa simples e funcional para Web
+	var url = "https://godot-python-game.onrender.com/execute_code"
 	
 	var error = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
 

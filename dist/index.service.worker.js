9c842246a63794f6eeba2aa547239df8890f20a9
Douglas Gabriel da Costa Alves <104402297+Kirikazu22@users.noreply.github.com>
2025-07-16 12:13:28 -0300

    Add files via upload

--- dist/index.service.worker.js
+++ dist/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1752676866|366619842";
+const CACHE_VERSION = "1752678417|63423995";
 const CACHE_PREFIX = "projeto_jogo_pyt-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
